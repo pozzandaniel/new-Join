5e0166154c69e7f054fe0d6d3799fa6d5db7116c
Daniel Pozzan <103948626+pozzandaniel@users.noreply.github.com>
2022-10-16 13:30:36 +0200

    function create Task

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 let logoutStatus = false;
 
+class Task {
+    constructor(
+        title,
+        description,
+        category,
+        assigned,
+        dueDate,
+        priority,
+        subtasks
+        ){
+        this.title = title;
+        this.description = description;
+        this.category = category;
+        this.assigned = assigned;
+        this.dueDate = dueDate;
+        this.priority = priority;
+        this.subtasks = subtasks;
+    };
+}
+
 
 
 async function init() {
@@ -33,15 +53,35 @@ function toggleLogout() {
 }
 
 
-function activeOption(){
+function activeCategory(){
     let category = document.getElementById('category');
     let newCategory = document.getElementById('newCategory');
     let inputIcons = document.getElementById('input-icons');
     if(category.value == 'new-category'){
        category.classList.add('d-none');
        newCategory.classList.remove('d-none');
-       inputIcons.classList.remove('d-none')
+       inputIcons.classList.remove('d-none');
     }
+}
+
+function activeContact() {
+    let assigned = document.getElementById('assigned');
+    let newContact = document.getElementById('newContact');
+    let inputIcons = document.getElementById('img-icons');
+    if(assigned.value == 'new-contact'){
+       assigned.classList.add('d-none');
+       newContact.classList.remove('d-none');
+       inputIcons.classList.remove('d-none');
+    }
+}
+
+function activeSubtask() {
+    let addSubtskIcon = document.getElementById('addSubtskIcon');
+    let subtask = document.getElementById('subtask');
+    let inputIcons = document.getElementById('subtasks-icons');
+    addSubtskIcon.classList.add('d-none');
+    subtask.classList.remove('d-none');
+    inputIcons.classList.remove('d-none');
 }
 
 
@@ -56,6 +96,26 @@ function cancelNewCategory() {
 
 }
 
+function cancelNewContact() {
+    let assigned = document.getElementById('assigned');
+    let newContact = document.getElementById('newContact');
+    let inputIcons = document.getElementById('img-icons');
+    newContact.value = '';
+    assigned.classList.remove('d-none');
+    newContact.classList.add('d-none');
+    inputIcons.classList.add('d-none');
+
+}
+
+function cancelNewSubtask() {
+    let addSubtskIcon = document.getElementById('addSubtskIcon');
+    let subtask = document.getElementById('subtask');
+    let inputIcons = document.getElementById('subtasks-icons');
+    subtask.value = '';
+    addSubtskIcon.classList.remove('d-none');
+    inputIcons.classList.add('d-none');
+}
+
 function addNewCategory() {
     let category = document.getElementById('category');
     let newCategory = document.getElementById('newCategory');
@@ -66,19 +126,75 @@ function addNewCategory() {
     category.classList.remove('d-none');
     newCategory.classList.add('d-none');
     inputIcons.classList.add('d-none');
+}
+
+function addNewContact() {
+    let assigned = document.getElementById('assigned');
+    let newContact = document.getElementById('newContact');
+    let inputIcons = document.getElementById('img-icons');
+    assigned.innerHTML += `<option value="${newContact.value.toLowerCase()}" >${newContact.value}</option>
+    `;
+    newContact.value = '';
+    assigned.classList.remove('d-none');
+    newContact.classList.add('d-none');
+    inputIcons.classList.add('d-none');
+
+}
+
+function addNewSubtask() {
+    let randomId = generateRandomId();
+    let addSubtskIcon = document.getElementById('addSubtskIcon');
+    let subtask = document.getElementById('subtask');
+    let value = generateValueFronValue(subtask);
+    let inputIcons = document.getElementById('subtasks-icons');
+    let subtasksBox = document.getElementById('subtasksBox');
+    subtasksBox.innerHTML += `<input type="checkbox" id="subtask${randomId}" name="subtask${randomId}" value="${value}" >
+    <label for="subtask${randomId}">${subtask.value}</label><br>
+    `;
 
 
 
 }
 
+function generateRandomId() {
+    
+    return Math.floor((1 + Math.random()) * 0x10000)
+    .toString(16)
+    .substring(1);
+    
+}
 
-// function logout() {
-//     signOut(auth).then(() => {
-                
-//         // Sign-out successful.
-//     }).catch((error) => {
-    //           // An error happened.
-    //     });
-//     window.location.href='login.html';
-// }
+function generateValueFronValue(subtask) {
+    let value = '';
+    let array =  subtask.value.split(' ');
+    for (let index = 0; index < array.length; index++) {
+        if(index < array.length-1){
+            const element = array[index] + '-';
+            value += element;
+        } else {
+            const endElement = array[index]
+            value += endElement;
+        }
+    }
+    return value.toLocaleLowerCase();
+}
+
+function createNewTask(){
+    event.preventDefault();
+    let title = document.getElementById('title').value;
+    let description = document.getElementById('description').value;
+    let category = document.getElementById('category').value;
+    let assigned = document.getElementById('assigned').value;
+    let dueDate = document.getElementById('dueDate').value;
+    let priority = document.getElementById('priority');
+
+    let newTask = new Task(title, description, category, assigned, dueDate);
+
+        
+
+
+}
+
+
+
 
